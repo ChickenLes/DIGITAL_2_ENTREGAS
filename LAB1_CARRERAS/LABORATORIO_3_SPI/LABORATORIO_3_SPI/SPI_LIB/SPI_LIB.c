062fdf52c44df01ef5dce4037c810f941179fc0e
Anthony Boteo <144646034+ChickenLes@users.noreply.github.com>
2026-02-03 11:22:58 -0600

    CREACIÓN LIBRERIA SPI
    CREACIÓN DE LA LIBRERIA SPI, NO SE UTILIZÓ POLLING, SIN EMBARGO NO SE A PROBADO LA LIBRERIA UTILIZANDO LA INTERRUPCIÓN.

---
+++ LAB1_CARRERAS/LABORATORIO_3_SPI/LABORATORIO_3_SPI/SPI_LIB/SPI_LIB.c
@@ -0,0 +1,114 @@
+/*
+ * SPI_LIB.c
+ *
+ * Created: 2/3/2026 10:34:56 AM
+ *  Author: itzle
+ */ 
+
+#include "SPI_LIB.h"
+
+//BUFFER PARA GUARDAR EL DATO DE LA INTERRUPCIÓN
+uint8_t BUFFER_RES = 0;
+uint8_t RES_FLAG = 1;
+
+ISR(SPI_STC_vect){
+	
+	BUFFER_RES = SPDR;
+	RES_FLAG = 1;
+}
+
+void SPI_INIT(SPI_TYPE type, SPI_ORDER dataOrder, CLOCK_POLARITY clockPolarity, CLOCK_PHASE clockPhase){
+	
+	if(type &(1<<MSTR))
+	{
+	 SPI_DDR |= (1<<SPI_MOSI)|(1<<SPI_SCK)|(1<<SPI_SS);
+	 
+	 SPI_DDR &= ~(1<<SPI_MISO);
+	 
+	 SPI_PORT |= (1<<SPI_SS);
+	}
+	else
+	{
+		SPI_DDR |= (1<<SPI_MISO);
+		
+		SPI_DDR &= ~((1<<SPI_MOSI)|(1<<SPI_SCK)|(1<<SPI_SS));
+	}
+	
+	SPCR = (1<<SPE)|(1<<SPIE)|dataOrder|clockPolarity|clockPhase;
+	
+	if(type &(1<<MSTR))
+	{
+	SPCR |= (1<<MSTR);
+	}
+	else
+	{
+		SPCR &= ~(1<<MSTR);
+	}
+	
+	uint8_t V_TEMP = type & 0b00000111;
+	switch(V_TEMP) {
+		// DIV2
+		case 0: 
+			SPCR &= ~((1<<SPR1)|(1<<SPR0)); 
+			SPSR |= (1<<SPI2X);            
+		break;
+		// DIV4
+		case 1: 
+			SPCR &= ~((1<<SPR1)|(1<<SPR0));
+			SPSR &= ~(1<<SPI2X);         
+		break;
+		// DIV8
+		case 2: 
+			SPCR |= (1<<SPR0);
+			SPCR &= ~(1<<SPR1);
+			SPSR |= (1<<SPI2X);            
+		break;
+		 // DIV16
+		case 3:
+			SPCR |= (1<<SPR0);
+			SPCR &= ~(1<<SPR1);
+			SPSR &= ~(1<<SPI2X);           
+		break;
+		// DIV32
+		case 4: 
+			SPCR &= ~(1<<SPR0);
+			SPCR |= (1<<SPR1);
+			SPSR |= (1<<SPI2X);             
+		break;
+		 // DIV64
+		case 5:
+			SPCR &= ~(1<<SPR0);
+			SPCR |= (1<<SPR1);
+			SPSR &= ~(1<<SPI2X);            
+		break;
+		// DIV128
+		case 6: 
+			SPCR |= (1<<SPR0)|(1<<SPR1);
+			SPSR &= ~(1<<SPI2X);            
+		break;
+	}
+	
+}
+
+
+void SPI_WRITE(uint8_t dato){
+	SPDR = dato;
+	while (!(SPDR &(1<<SPIF)));
+}
+
+uint8_t SPI_READ(void){
+	
+	RES_FLAG = 0;
+	return BUFFER_RES; //LEEMOS EL ÚLTIMO DATO CAPTURADO POR LA ISR
+	
+}
+
+uint8_t SPI_RECEIVE_CHECK(void){
+	return RES_FLAG;
+	
+}
+
+
+
+
+
---
+++ LAB1_CARRERAS/LABORATORIO_3_SPI/LABORATORIO_3_SPI/main.c
@@ -0,0 +1,20 @@
+/*
+ * LABORATORIO_3_SPI.c
+ *
+ * Created: 2/3/2026 10:33:04 AM
+ * Author : itzle
+ */ 
+
+
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
